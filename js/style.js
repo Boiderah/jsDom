b0e8933ec3ed44b0edd8d6a2f27691e6dcbbcf7c
emmanuel okezie <emmanuelokezie50@gmail.com>
2024-02-14 22:52:21 +0100

    first commit

---
+++ js/style.js
@@ -0,0 +1,14 @@
+let design= document.getElementById('part')
+console.log(part);
+
+let winner= document.getElementsByClassName('textnet')
+console.log(winner);
+
+let whyne= document.getElementsByTagName('h1')
+console.log(whyne);
+
+let add= document.querySelector('h1')
+console.log(add);
+
+let whyt= document.querySelectorAll('h1')
+console.log(whyt);
