17c453cff9c9882712e443b73f75620f7ddac90d
emmanuel okezie <emmanuelokezie50@gmail.com>
2024-02-15 17:40:34 +0100

    added function

--- js/style.js
+++ js/style.js
@@ -1,4 +1,5 @@
 let design= document.getElementById('part')
+const gabb= design.innerHTML
 console.log(part);
 
 let winner= document.getElementsByClassName('textnet')
@@ -12,3 +13,34 @@ console.log(add);
 
 let whyt= document.querySelectorAll('h1')
 console.log(whyt);
+
+design.style.color=`red`;
+design.style.fontSize= `40px`;
+// design.innerHTML = `i am <em>good to go</em> trust me`
+// let boi = document.getElementById('win')
+// boi.removeChild(design)
+
+let changeimg = document.getElementById('dera')
+// changeimg.onclick= boi
+changeimg.addEventListener('click', boi)
+
+// function boi() {
+//     if (design.innerHTML== 'lets try and see') {
+//         design.innerHTML='i am <em>good to go</em> trust me'
+//     } else {
+//         design.innerHTML= 'lets try and see'
+//     }
+//     changeimg.setAttribute('src', './res/img_7816.jpg')
+// }
+function boi() {
+    if (design.innerHTML== gabb) {
+        design.innerHTML='i am <em>good to go</em> trust me'
+    } else {
+        design.innerHTML= gabb
+    } else if {
+        design.innerHTML= 'billionaire'
+    }
+
+
+    changeimg.setAttribute('src', './res/img_7816.jpg')
+}
